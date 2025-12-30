34f17770fabd2860a86645041e680f71f7db8395
Prashant Bhadu <prashantbhadu97@gmail.com>
2025-12-30 10:56:11 +0530

    blogs app and blog model

---
+++ blogs/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from .models import Category ,Blog
+
+
+class BlogAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('tittle',)}
+    list_display= ('tittle', 'category', 'status','author', 'is_featured')
+    search_fields = ('id', 'tittle', 'category__category_name', 'status')
+    list_editable=('is_featured',)
+
+
+
+admin.site.register(Category)
+admin.site.register(Blog, BlogAdmin)
