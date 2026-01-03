e2339f40bcf9235a74f2f2b8cc7f9069fce55843
Prashant Bhadu <prashantbhadu97@gmail.com>
2026-01-03 16:23:49 +0530

    posts by category

--- blog_main/urls.py
+++ blog_main/urls.py
@@ -19,8 +19,12 @@ from django.urls import include, path
 from . import views
 from django.conf.urls.static import static
 from django.conf import settings
+from blogs import views as BlogsView # we cann't have the same name as we have imported views from blog and blog_main as well
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('category/', include('blogs.urls')),
+    path('<slug:slug>/', BlogsView.blogs, name='blogs'),
+    # Search endpoint
+    path('blogs/search/', BlogsView.search, name='search'),
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
